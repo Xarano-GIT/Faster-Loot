66ab3a3685e7cb228bd0a9be974bc4928eb4bd57
Xarano-GIT <admin@xarano.uk>
2025-12-18 10:11:05 +0000

    Updated Midnight Beta TOC Added support for TSM Destroy

--- FasterLoot/FasterLoot.lua
+++ FasterLoot/FasterLoot.lua
@@ -8,10 +8,13 @@ addon:RegisterEvent("LOOT_READY")
 addon:SetScript("OnEvent", function()
     if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
         if (GetTime() - epoch) >= LOOT_DELAY then
+            if TSMDestroyBtn and TSMDestroyBtn:IsShown() and TSMDestroyBtn:GetButtonState() == "DISABLED" then epoch = GetTime() return
+            end
             for i = GetNumLootItems(), 1, -1 do
                 LootSlot(i)
             end
             epoch = GetTime()
         end
     end
-end)
+end
+)
